32f30dcc1a4fc69c107ef2390c1f5ea823a5fb58
Error323 <folkerthuizinga@gmail.com>
2014-07-10 23:06:57 +0200

    update start script

--- start.sh
+++ start.sh
@@ -2,11 +2,13 @@
 
 TIME=5
 
-trap "killall -u ${USER} -9 nc; exit" SIGINT SIGTERM
+trap "killall -u ${USER} -9 sdohdr" SIGINT SIGTERM
 
 for port in 53268 53276 53284 53292 53300 53308
 do 
-  nc -u -l 10.99.100.1 $port | ./sdohdr -f $port.bin -t $TIME -i &
+  cmd="sdohdr -f $port.bin -t $TIME -u $port"
+  echo $cmd
+  ./$cmd &
 done
 
 wait
