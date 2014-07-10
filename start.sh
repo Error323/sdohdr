c2e5c49297b54074030e8d20a8149f11c4665297
Error323 <folkerthuizinga@gmail.com>
2014-07-10 23:39:09 +0200

    add utc time to start script

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ trap "killall -u ${USER} -9 sdohdr" SIGINT SIGTERM
 
 for port in 53268 53276 53284 53292 53300 53308
 do 
-  cmd="sdohdr -f $port.bin -t $TIME -u $port"
+  cmd="sdohdr -f port$port-$(date -u +"utc%Y%m%d%H%M%S").bin -t $TIME -u $port"
   echo $cmd
   ./$cmd &
 done
